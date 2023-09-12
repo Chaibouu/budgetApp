90b7284949a78905b7fd1b813b817442cbd1ae4b
Chaibouu <chaibouabdoulwahab@gmail.com>
2023-09-12 02:09:27 +0100

    add budget

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-let inputbudget = document.querySelector('.inputbudget');
-console.log(inputbudget);
+let inputbudget = document.querySelector('#inputbudget');
+let inputexpense = document.querySelector('#inputexpense');
+let inputAmount = document.querySelector('#inputAmount');
+let btncalculate = document.querySelector('#btncalculate');
+let btnexpense = document.querySelector('#btnexpense');
+let chfbudget = document.querySelector('.chfbudget');
+let chfexpense = document.querySelector('.chfexpense');
+let chfbalance = document.querySelector('.chfbalance');
+
+// Ajouter un budget
+btncalculate.addEventListener('click', () =>{
+    chfbudget.textContent = inputbudget.value + ' F CFA'
+    inputbudget.value = '';
+})
+// Ajouter une dépence
+btnexpense.addEventListener('click', () =>{
+    chfbudget.textContent = parseInt(chfbudget.textContent) - inputexpense.value + ' F CFA';
+    chfexpense.textContent =inputexpense.value + ' F CFA';
+    chfbalance.textContent = inputAmount.value;
+    inputexpense.value = '';
+    inputAmount.value = '';
+})
