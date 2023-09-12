815ebf5e3adf0f49f5124fc0daefc7ad18045f90
Chaibouu <chaibouabdoulwahab@gmail.com>
2023-09-12 21:40:06 +0100

    add budget

--- index.js
+++ index.js
@@ -8,35 +8,42 @@ let chfexpense = document.querySelector('.chfexpense');
 let chfbalance = document.querySelector('.chfbalance');
 
 // Création de l'objet pour stocker les valeur
-// const valeurr = JSON.parse(localStorage.getItem('valeur'));
 const valeur = {
-    budget : parseInt(inputbudget.value),
-    expense : parseInt(inputexpense.value),
-    amount : parseInt(inputAmount.value),
+    budget : '',
+    expense : '',
+    balance : '',
 };
-localStorage.setItem('valeur', JSON.stringify(valeur));
-let j = JSON.parse(localStorage.getItem('valeur'));
-console.log(j);
-chfbudget.textContent = localStorage.getItem('budget'); + ' F CFA'
+
 // Ajouter un budget
 btncalculate.addEventListener('click', () =>{
     if (inputbudget.value !== '') {
-        // localStorage.setItem('budget', inputbudget.value);
-        localStorage.setItem('budget', JSON.stringify(valeur.budget)) = j.budget + inputbudget; 
-        let i = localStorage.getItem('budget') + parseInt(valeur.budget);
-        localStorage.setItem('budget',JSON.stringify(i))
-        inputbudget.value = '';
-    }
+        if (localStorage.getItem('valeur')) {
+            let valeurr = JSON.parse(localStorage.getItem('valeur'));
+            let bbudget = valeurr.budget;
+            valeur.budget = Number(inputbudget.value) + Number(bbudget);
+            localStorage.setItem('valeur',JSON.stringify(valeur));
+        } else {
+            valeur.budget = inputbudget.value;
+            localStorage.setItem('valeur',JSON.stringify(valeur));
+        }
+    } 
     else{
         alert('Veuillez entrer un montant pour le Budget')
     }
 })
+
+
 // Ajouter une dépence
 btnexpense.addEventListener('click', () =>{
-    chfbudget.textContent = parseInt(chfbudget.textContent) - inputexpense.value + ' F CFA';
-    localStorage.setItem('expense', inputexpense.value);
-    chfexpense.textContent =inputexpense.value + ' F CFA';
-    chfbalance.textContent = inputAmount.value;
-    inputexpense.value = '';
-    inputAmount.value = '';
+    if (inputexpense.value !== '') {
+        if (inputAmount.value !== '') {
+        
+        } 
+        else{
+            alert('Veuillez remplir le champ de description de la dépence ')
+        }
+    } 
+    else{
+        alert('Veuillez remplir le champ pour une dépence')
+    }
 })
