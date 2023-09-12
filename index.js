79aad989ca96c0d41d8709cf6bcfe36a9bd72302
Chaibouu <chaibouabdoulwahab@gmail.com>
2023-09-12 14:35:32 +0100

    add budget

--- index.js
+++ index.js
@@ -8,17 +8,23 @@ let chfexpense = document.querySelector('.chfexpense');
 let chfbalance = document.querySelector('.chfbalance');
 
 // Création de l'objet pour stocker les valeur
-const valeurr = JSON.parse(localStorage.getItem('valeur'));
+// const valeurr = JSON.parse(localStorage.getItem('valeur'));
 const valeur = {
-    budget : inputbudget.value,
-    expense : inputexpense.value,
-    amount : inputamount.value,
+    budget : parseInt(inputbudget.value),
+    expense : parseInt(inputexpense.value),
+    amount : parseInt(inputAmount.value),
 };
-
+localStorage.setItem('valeur', JSON.stringify(valeur));
+let j = JSON.parse(localStorage.getItem('valeur'));
+console.log(j);
+chfbudget.textContent = localStorage.getItem('budget'); + ' F CFA'
 // Ajouter un budget
 btncalculate.addEventListener('click', () =>{
     if (inputbudget.value !== '') {
-        chfbudget.textContent = inputbudget.value + ' F CFA'
+        // localStorage.setItem('budget', inputbudget.value);
+        localStorage.setItem('budget', JSON.stringify(valeur.budget)) = j.budget + inputbudget; 
+        let i = localStorage.getItem('budget') + parseInt(valeur.budget);
+        localStorage.setItem('budget',JSON.stringify(i))
         inputbudget.value = '';
     }
     else{
@@ -28,6 +34,7 @@ btncalculate.addEventListener('click', () =>{
 // Ajouter une dépence
 btnexpense.addEventListener('click', () =>{
     chfbudget.textContent = parseInt(chfbudget.textContent) - inputexpense.value + ' F CFA';
+    localStorage.setItem('expense', inputexpense.value);
     chfexpense.textContent =inputexpense.value + ' F CFA';
     chfbalance.textContent = inputAmount.value;
     inputexpense.value = '';
