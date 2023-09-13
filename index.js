4110806081b8e5a32646b2871bcbea50440d4bb2
Chaibouu <chaibouabdoulwahab@gmail.com>
2023-09-13 12:35:33 +0100

    add budget

--- index.js
+++ index.js
@@ -3,17 +3,26 @@ let inputexpense = document.querySelector('#inputexpense');
 let inputAmount = document.querySelector('#inputAmount');
 let btncalculate = document.querySelector('#btncalculate');
 let btnexpense = document.querySelector('#btnexpense');
+let btnreset = document.querySelector('.btnreset');
 let chfbudget = document.querySelector('.chfbudget');
 let chfexpense = document.querySelector('.chfexpense');
 let chfbalance = document.querySelector('.chfbalance');
 let containerLibelle = document.querySelector('.containerLibelle');
-
+let supprime = document.querySelector('.supprime');
+let editer = document.querySelector('.editer');
 // Création de l'objet pour stocker les valeur
 const valeur = {
     budget : '',
     expense : '',
     balance : '',
 };
+
+
+//button reset value
+btnreset.addEventListener('click', () =>{
+ localStorage.clear();
+ document.location.reload();
+})
 
 
 // Création de l'objet pour stocker les libelles de dépence
@@ -23,10 +32,39 @@ if (!localStorage.getItem('cles')) {
 }
 
 // Affectation des valeurs dans label
+if (!localStorage.getItem('valeur')) {
+    localStorage.setItem('valeur', JSON.stringify(valeur))
+}
     let valeurr = JSON.parse(localStorage.getItem('valeur'));
     chfbudget.textContent = valeurr.budget + ' F';
     chfexpense.textContent = valeurr.expense + ' F';
     chfbalance.textContent = valeurr.balance + ' F';
+
+// Affichage des libelles
+let tabb = JSON.parse(localStorage.getItem('cles'));
+tabb.forEach(element => {
+    let boite = document.createElement('div');
+    containerLibelle.append(boite);
+    boite.classList.add('boite');
+    let para1 = document.createElement('p');
+    para1.setAttribute('class', 'para1');
+    boite.append(para1)
+    let para2 = document.createElement('p');
+    boite.append(para2)
+    let sup = document.createElement('span');
+    sup.setAttribute('class','supprime');
+    let edit = document.createElement('span');
+    edit.setAttribute('class','editer');
+    let para3 = document.createElement('p');
+    sup.innerHTML = `<i class="fa-solid fa-trash-can" style="color: #cf2029;"></i>`;
+    edit.innerHTML = `<i class="fa-solid fa-pen-to-square" style="color: #6698f0;"></i>`
+    para3.append(sup);
+    para3.append(edit);
+    boite.append(para3);
+    para1.innerHTML = element.titre;
+    para2.innerHTML = element.valu;
+   });
+
 // Ajouter un budget
 btncalculate.addEventListener('click', () =>{
     if (inputbudget.value !== '') {
@@ -40,7 +78,8 @@ btncalculate.addEventListener('click', () =>{
             localStorage.setItem('valeur',JSON.stringify(valeur));
             document.location.reload();
         } else {
-            valeur.budget = inputbudget.value;
+            let valeurr = JSON.parse(localStorage.getItem('valeur'));
+            valeur.budget = Number(inputbudget.value);
             localStorage.setItem('valeur',JSON.stringify(valeur));
             document.location.reload();
         }
@@ -85,32 +124,22 @@ btnexpense.addEventListener('click', () =>{
 //Fonction permettant d'entrer les libellés    
 const insert = ()=>{
       // insertion des Libelles
-      let boite = document.createElement('div');
-      containerLibelle.append(boite);
-      boite.classList.add('boite');
-      let para1 = document.createElement('p');
-      boite.append(para1)
-      let para2 = document.createElement('p');
-
-      boite.append(para2)
-      let para3 = document.createElement('p');
-      para3.textContent = 'icone';
-      boite.append(para3)
-
-      const libel = {
-        titre : inputAmount.value,
-        valu : inputexpense.value,
+        const libel = {
+            titre : inputAmount.value,
+            valu : inputexpense.value,
         }
         let tabb = JSON.parse(localStorage.getItem('cles'));
-
         tabb.push(libel);
         localStorage.setItem('cles',JSON.stringify(tabb));
-
-
-      let libb = JSON.parse(localStorage.getItem('cles'));
-    
-        para1.innerHTML = libb.titre;
-        para2.innerHTML = libb.valu;
-
-
 }
+
+// button supprimer un libellé
+// supprime.addEventListener('click',()=>{
+//     localStorage.removeItem("key");
+// })
+
+
+// button editer un libellé
+// editer.addEventListener('click',()=>{
+//     boite.style.backgroundColor = 'red'
+// })
